0f58dd01f75a10555870c038152cb21e7f36d9d8
Anushka Khare <67369620+AnushkaKhare786@users.noreply.github.com>
2022-04-29 21:22:29 +0530

    Anushka Khare

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ document.querySelector('#menu-btn').onclick = () =>{
     cartItem.classList.remove('active');
 }
 
+var id = 0;
+function addToCart(variant, image) {
+	var cartItem = `<div class="cart-item" id="${id}">
+	<span class="fas fa-times" onclick="remove(${id})"></span>
+	<img src="${image}" alt="Doppio">
+	<div class="content">
+	<h3>${variant}</h3>
+	<div class="price">$15.99/-</div> 
+	</div>
+	</div>`;
+	id++;
+	const cartContainer = document.querySelector('#cart-items-container');
+	cartContainer.innerHTML += cartItem;
+
+}
+
+function remove(id) {
+	const element = document.getElementById(`${id}`);
+	element.remove();
+}
+
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
